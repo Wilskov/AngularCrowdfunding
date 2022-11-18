6e40011958e1e528f80d12494e26442916f587dd
akwakwak6 <misson_pol@hotmail.fr>
2022-11-18 09:39:33 +0100

    add register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componants/login/login.component';
+import { RegisterComponent } from './componants/register/register.component';
 
 const routes: Routes = [
-  { path : "", component : LoginComponent}
+  { path : "", component : RegisterComponent},
+  { path : "login", component : LoginComponent}
 ];
 
 @NgModule({
---
+++ src/app/componants/register/register.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm : FormGroup;
+
+  constructor(private _fb : FormBuilder,
+              private _authService : AuthService,
+              private _router : Router) { 
+                
+      this.registerForm = _fb.group({
+        nickname : [null, [Validators.required]],
+        email : [null, [Validators.required, Validators.email]],
+        password : [null, [Validators.required] ],//,Validators.minLength(8), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W|_]).+$/)]
+        confirmPassword : [null, [Validators.required]],
+        birtDay : [null, [Validators.required]],
+        role : [[], [Validators.required]]
+        }, { validators : [ this.passwordValidator ] })
+  }
+
+  ngOnInit(): void {
+  }
+
+  register() : void {
+    console.log(this.registerForm.value)
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value)     
+    }
+    else {
+      this.registerForm.markAllAsTouched()
+    }
+  }
+
+  passwordValidator(group : AbstractControl): ValidationErrors | null {
+
+    if(group.get('password')?.value == "" || group.get('confirmPassword')?.value == "") 
+      return null
+    
+    if(group.get('password')?.value === group.get('confirmPassword')?.value)
+      return null
+    
+    return { notsame : true }
+
+    /*
+
+    if(group.get('password')?.value != "" && group.get('confirmPassword')?.value != ""){
+      if(group.get('password')?.value != group.get('confirmPassword')?.value){
+        return { notsame : true }
+      }
+      return null;
+    }
+    return null;    */
+  }
+
+}
