d76edd30c1f9f505d015434aa1a42060e730e4bb
Philémon Tchatchoua Kamga <p.tchatchouakamga@stag.technofuturtic.education>
2022-11-21 16:08:14 +0100

    regsiter et login ok

--- src/app/componants/register/register.component.ts
+++ src/app/componants/register/register.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IRole } from 'src/app/Interface/role';
 import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
+
 export class RegisterComponent implements OnInit {
+
+ typeDeRole : IRole[] = [
+  {id:2, role:"Contributeur"},
+  {id:3, role:"projectOwner"},
+ ]
+
+
+
 
   registerForm : FormGroup;
 
   constructor(private _fb : FormBuilder,
               private _authService : AuthService,
-              private _router : Router) { 
-                
+              private _router : Router) {
+
       this.registerForm = _fb.group({
         nickname : [null, [Validators.required]],
         email : [null, [Validators.required, Validators.email]],
@@ -27,12 +39,13 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
 
   register() : void {
     console.log(this.registerForm.value)
     if(this.registerForm.valid){
-      console.log(this.registerForm.value)     
+      console.log(this.registerForm.value)
     }
     else {
       this.registerForm.markAllAsTouched()
@@ -41,12 +54,12 @@ export class RegisterComponent implements OnInit {
 
   passwordValidator(group : AbstractControl): ValidationErrors | null {
 
-    if(group.get('password')?.value == "" || group.get('confirmPassword')?.value == "") 
+    if(group.get('password')?.value == "" || group.get('confirmPassword')?.value == "")
       return null
-    
+
     if(group.get('password')?.value === group.get('confirmPassword')?.value)
       return null
-    
+
     return { notsame : true }
 
     /*
