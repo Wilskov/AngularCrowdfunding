1d873d5c5b44d59e35f94e8e237721ea53919645
akwakwak6 <misson_pol@hotmail.fr>
2022-11-22 11:27:58 +0100

    sort import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './componants/login/login.component';
+import { CreatePalierFormComponent } from './componants/create-projet-form/create-palier-form/create-palier-form.component';
+import { CreateProjetFormComponent } from './componants/create-projet-form/create-projet-form.component';
 import { RegisterComponent } from './componants/register/register.component';
-import { HomeComponent } from './componants/home/home.component';
 import { NavbarComponent } from './componants/navbar/navbar.component';
 import { FooterComponent } from './componants/footer/footer.component';
-
-import { CreateProjetFormComponent } from './componants/create-projet-form/create-projet-form.component';
-import { CreatePalierFormComponent } from './componants/create-projet-form/create-palier-form/create-palier-form.component';
+import { LoginComponent } from './componants/login/login.component';
+import { HomeComponent } from './componants/home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -35,3 +34,30 @@ import { CreatePalierFormComponent } from './componants/create-projet-form/creat
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
